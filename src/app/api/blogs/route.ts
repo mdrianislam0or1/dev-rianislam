a5a028a628cce8f5c7d85acb8fe3bb0d22567861
mdrianislam0or1 <rianislamdr@gmail.com>
2024-07-27 22:30:46 +0600

    add more

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,5 +1,5 @@
 import { dbConnection } from "@/lib/dbConnection";
-import Order from "@/lib/models/blogModel";
+import Blog from "@/lib/models/blogModel";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -7,8 +7,8 @@ export async function POST(request: NextRequest) {
 
   try {
     const data = await request.json();
-    const order = await Order.create(data);
-    return NextResponse.json({ success: true, data: order }, { status: 201 });
+    const blog = await Blog.create(data);
+    return NextResponse.json({ success: true, data: blog }, { status: 201 });
   } catch (error) {
     return NextResponse.json({ success: false, error }, { status: 400 });
   }
@@ -18,8 +18,8 @@ export async function GET(request: NextRequest) {
   await dbConnection();
 
   try {
-    const products = await Order.find({});
-    return NextResponse.json({ success: true, data: products });
+    const projects = await Blog.find({});
+    return NextResponse.json({ success: true, data: projects });
   } catch (error) {
     return NextResponse.json({ success: false, error }, { status: 400 });
   }
